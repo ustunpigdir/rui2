74ede4a1ad0e603ecb10497ac1dba49cdfb98d5c
Ustunpasa Igdir <76748413+ustunpigdir@users.noreply.github.com>
2021-12-02 10:42:18 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,76 @@
+/* Theme Name: Lizehen  - Responsive Business & Agency Template
+   Author: Themesdesign
+   Version: 1.0.0
+   File Description: Main JS file of the template
+*/
+
+// STICKY
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 50) {
+        $(".sticky").addClass("nav-sticky");
+    } else {
+        $(".sticky").removeClass("nav-sticky");
+    }
+});
+
+
+// SmoothLink
+$('.smoothlink').on('click', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 0
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+});
+
+
+$(".testi-carousel").owlCarousel({
+    autoPlay: 3000,
+    stopOnHover: true,
+    navigation: false,
+    paginationSpeed: 1000,
+    goToFirstSpeed: 2000,
+    singleItem: true,
+    autoHeight: true,
+});
+
+
+//  VIDEO 
+$('.video-play-icon').magnificPopup({
+    disableOn: 700,
+    type: 'iframe',
+    mainClass: 'mfp-fade',
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: false
+});
+
+
+// Magnific Popup
+$('.mfp-image').magnificPopup({
+    type: 'image',
+    closeOnContentClick: true,
+    mainClass: 'mfp-fade',
+    gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        preload: [0, 1]
+    }
+});
+
+// typed
+$(".element").each(function(){
+    var $this = $(this);
+    $this.typed({
+        strings: $this.attr('data-elements').split(','),
+        typeSpeed: 100, // typing speed
+        backDelay: 3000 // pause before backspacing
+    });
+});
+
+// video
+$(".player").mb_YTPlayer();
+
+
